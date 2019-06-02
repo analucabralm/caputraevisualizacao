eff9e1688346d3553d0bb8147c6bca0109188df8
analucabralm <50282486+analucabralm@users.noreply.github.com>
2019-06-01 21:22:29 -0300

    Add files via upload

---
+++ MercadoLivre.py
@@ -0,0 +1,33 @@
+#Importando o scrapy
+#Usando o recurso de abrir no browser para pegar as classes dos elementos
+import scrapy
+from scrapy.utils.response import open_in_browser
+
+
+class MercadoSpider(scrapy.Spider):
+    #Nomeando a classe
+    name = "MercadoSpider"
+
+    def __init__(self, produto="", **kwargs):
+        self.start_urls = {"https://lista.mercadolivre.com.br/%s" % produto}
+        super().__init__(**kwargs)
+
+    
+    def parse(self, response):
+        nomes_produtos = response.css(".main-title::text").extract()
+        preco_produtos = []
+
+        for item in response.css(".item__price"):
+            preco = item.css(".price__fraction::text").extract_first()
+
+            if item.css(".price__decimals::text").extract_first():
+                preco += "," + item.css(".price__decimals::text").extract_first()
+
+            preco_produtos.append(preco)
+
+        for nome, preco in zip(nomes_produtos, preco_produtos):
+            yield {
+                "nome" : nome,
+                "preco" : preco
+            }
+        pagination = response.xpath("//ul[@role]/li[last()]/a")
